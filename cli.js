765d09cc1a8c640bdb0625c1d2b355a1619f95a5
Brandon Reavis <brandon@brandonreavis.com>
2014-11-16 22:46:58 -0700

    Added basic CLI script.

---
+++ cli.js
@@ -0,0 +1,16 @@
+var geocsvinfo = require('./index.js');
+
+var filename = process.argv[2];
+if (!filename) {
+	console.error('Filename must be provided');
+	process.exit(1);
+}
+
+geocsvinfo(filename, function(err, metadata){
+	if(err){
+		console.log(err);
+		process.exit(1);
+	}
+	console.log(metadata);
+	process.exit(0);
+});
