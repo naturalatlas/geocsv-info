44923b7e0efadfce9853defdaee675038226ce73
Brian Reavis <brian@thirdroute.com>
2014-11-17 11:41:54 -0700

    Removed coverage folder, fixed global leak.

--- geometry_parsers.js
+++ geometry_parsers.js
@@ -2,13 +2,13 @@
  * Methods that return a flattened coordinate array
  *
  * Note: these could use some optimizing
- * 
+ *
  * 2D: [x0, y0, x1, y1]
  * 3D: [x0, y0, z0, x1, y1, z1]
  */
 module.exports = {
 
-	WKT: function(str){
+	WKT: function(str) {
 		str = str.substring(str.indexOf('('), str.lastIndexOf(')')).replace(/\ {2,}/g, ' ');
 
 		var is3d = /[\w\-\+\.]\s[\w\-\+\.]+\s[\w\-\+\.]/.test(str);
@@ -18,21 +18,21 @@ module.exports = {
 
 		return {
 			dim: is3d ? 3 : 2,
-			arr: JSON.parse('['+str+']')
-		}
+			arr: JSON.parse('[' + str + ']')
+		};
 	},
 
-	GeoJSON: function(str){
+	GeoJSON: function(str) {
 		str = str.substring(str.indexOf('['), str.lastIndexOf(']')).replace(/ /g, '');
-		
+
 		var is3d = /[\w\-\+\.],[\w\-\+\.]+,[\w\-\+\.]/.test(str);
 
-		str = str.replace(/[\[\]]/g, '').replace(/\,{2,}/g, ','); 
+		str = str.replace(/[\[\]]/g, '').replace(/\,{2,}/g, ',');
 
 		return {
 			dim: is3d ? 3 : 2,
-			arr: JSON.parse('['+str+']')
-		}
+			arr: JSON.parse('[' + str + ']')
+		};
 	}
 
 };
--- index.js
+++ index.js
@@ -13,20 +13,22 @@ module.exports = function(filename, options, callback) {
 	options = options || {};
 
 	fs.stat(filename, function(err, stats) {
-		if(err) return callback(err);
+		if (err) return callback(err);
 
 		var filesize = stats.size;
 		var estimate = options.estimate;
 		var row_estimate = 0;
 		var rows_to_skip = 0;
-		var warning  = null;
+		var warning = null;
 		var has_json = false;
-		var first    = true;
-		var exited   = false;
+		var first = true;
+		var exited = false;
+
 		var minX = Number.POSITIVE_INFINITY;
 		var minY = Number.POSITIVE_INFINITY;
 		var maxX = Number.NEGATIVE_INFINITY;
 		var maxY = Number.NEGATIVE_INFINITY;
+
 		var getExtent;
 		var info = {
 			count: 0,
@@ -46,40 +48,41 @@ module.exports = function(filename, options, callback) {
 		var geocsvparser = through2(function(line, enc, callback){
 			line = line.toString();
 
-			if(first){
+			if (first) {
 				first = false;
 
-				//Detect separator
+				// Detect separator
 				info.separator = options.separator || module.exports.detectSeparator(line);
-				if(!info.separator){
+				if (!info.separator) {
 					return callback("Can't determine CSV separator");
 				}
 				csvparser.separator = new Buffer(info.separator)[0];
 
-				//Detect fields
+				// Detect fields
 				info.headers = line.split(info.separator);
 
-				//Detect geometry field
+				// Detect geometry field
 				info.geometryField = module.exports.detectGeometryField(info.headers);
-				if(!info.geometryField){
+				if (!info.geometryField) {
 					return callback("Unable to determine geometry field in CSV");
 				}
-				has_json  = info.geometryField.encoding === 'GeoJSON';
+
+				has_json = info.geometryField.encoding === 'GeoJSON';
 				getExtent = module.exports.getExtentParser(info.geometryField);
 			} else {
 				info.count++;
 
-				if(estimate){
-				 	if(rows_to_skip-- < 1){
-						row_estimate += filesize/(Math.max(line.length,10));
-						if(info.count > 1) row_estimate /= 2;
+				if (estimate) {
+				 	if (rows_to_skip-- < 1) {
+						row_estimate += filesize / (Math.max(line.length,10));
+						if (info.count > 1) row_estimate /= 2;
 						rows_to_skip = Math.floor(row_estimate / estimate);
 					} else {
 						return callback();
 					}
 				}
 
-				if(has_json) {
+				if (has_json) {
 					try {
 						line = module.exports.fixJSONQuoting(line);
 					} catch(err) {
@@ -97,12 +100,12 @@ module.exports = function(filename, options, callback) {
 
 		var stream = byline(filestream).pipe(geocsvparser).pipe(csvparser);
 
-		stream.on('data', function(row_obj){
+		stream.on('data', function(row_obj) {
 			info.tested++;
 
-			if(!info.fields){
+			if (!info.fields) {
 				info.fields = {};
-				info.headers.forEach(function(fieldname){
+				info.headers.forEach(function(fieldname) {
 					var is_number = /^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/.test(row_obj[fieldname]);
 					info.fields[fieldname] = is_number ? 'Number' : 'String';
 				});
@@ -112,11 +115,11 @@ module.exports = function(filename, options, callback) {
 			try {
 				extent = getExtent(row_obj);
 			} catch(err) {
-				warning = "Invalid geometry at feature "+(info.count-1);
+				warning = "Invalid geometry at feature " + (info.count - 1);
 				extent  = null;
 			}
 
-			if(extent){
+			if (extent) {
 				minX = Math.min(minX, extent.minX);
 				minY = Math.min(minY, extent.minY);
 				maxX = Math.max(maxX, extent.maxX);
@@ -138,14 +141,14 @@ module.exports = function(filename, options, callback) {
 }
 
 module.exports.getExtentParser = function(geometryField){
-	if(geometryField.encoding === 'PointFromColumns') {
+	if (geometryField.encoding === 'PointFromColumns') {
 		var field_x = geometryField.name.x;
 		var field_y = geometryField.name.y;
 		return function(row){
 			var x = parseFloat(row[field_x]);
 			var y = parseFloat(row[field_y]);
 			return {minX: x, minY: y, maxX: x, maxY: y};
-		}
+		};
 	} else {
 		var parseCoordinates = coordinateParsers[geometryField.encoding];
 		var field_geom = geometryField.name;
@@ -153,16 +156,16 @@ module.exports.getExtentParser = function(geometryField){
 			var coordinates;
 			coordinates = parseCoordinates(row[field_geom])
 			return getArrayExtent(coordinates);
-		}
+		};
 	}
-}
+};
 
-module.exports.detectGeometryField = function(fieldnames){
-	//adapted from: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L293
+module.exports.detectGeometryField = function(fieldnames) {
+	// adapted from: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L293
 	var result;
-	for(var i = 0; i<fieldnames.length; i++){
+	for (var i = 0, n = fieldnames.length; i < n; i++) {
 		var name = fieldnames[i].toLowerCase();
-		if(name == 'wkt' || name.indexOf('geom') > -1) {
+		if (name === 'wkt' || name.indexOf('geom') > -1) {
 			result = {
 				id: i,
 				name: fieldnames[i],
@@ -170,7 +173,7 @@ module.exports.detectGeometryField = function(fieldnames){
 			}
 			break;
 		}
-		if(name == 'geojson') {
+		if (name === 'geojson') {
 			result = {
 				id: i,
 				name: fieldnames[i],
@@ -178,7 +181,7 @@ module.exports.detectGeometryField = function(fieldnames){
 			};
 			break;
 		}
-		if(name == 'x'
+		if (name == 'x'
 			|| name == 'lon'
 			|| name == 'lng'
 			|| name == 'long'
@@ -194,7 +197,7 @@ module.exports.detectGeometryField = function(fieldnames){
 			result.name.x = fieldnames[i];
 			continue;
 		}
-		if(name == 'y'
+		if (name == 'y'
 			|| name == 'lat'
 			|| name.indexOf('latitude') > -1)
 		{
@@ -210,38 +213,36 @@ module.exports.detectGeometryField = function(fieldnames){
 		}
 	}
 	return result;
-}
+};
 
 module.exports.detectSeparator = function(csv_line) {
-	//implemented like: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L209
+	// implemented like: https://github.com/mapnik/mapnik/blob/f42805a5321d42f59b447a70f459058cf2c6cd5c/plugins/input/csv/csv_datasource.cpp#L209
 
 	var separators = [',','\t','|',';'];
 	var counts = separators.map(function(separator){
-		return csv_line.split(separator).length-1;
+		return csv_line.split(separator).length - 1;
 	});
 
 	var best = null, max = 0;
-	for(i=0; i<4; i++){
-		if(counts[i] > max){
+	for (var i = 0; i < 4; i++) {
+		if (counts[i] > max) {
 			max = counts[i];
 			best = separators[i];
 		}
 	}
 	return best;
-}
+};
 
 module.exports.fixJSONQuoting = function(csv_line) {
-	//normalizes geometry quoting to 'filebakery style' which is supported by csv-parser
-
+	// normalizes geometry quoting to 'filebakery style' which is supported by csv-parser
 	var chunks = csv_line.match(/(.*)["']{(.*)}['"](.*)/);
-
-	if(chunks.length > 2){
-		chunks[2] = '"{'+chunks[2].replace(/\\?"+/g,'""')+'}"';
-		return (chunks[1]||'')+chunks[2]+(chunks[3]||'');
+	if (chunks.length > 2) {
+		chunks[2] = '"{' + chunks[2].replace(/\\?"+/g,'""') + '}"';
+		return (chunks[1] || '') + chunks[2] + (chunks[3] || '');
 	} else {
 		return csv_line;
 	}
-}
+};
 
 function getArrayExtent(coords) {
 	var dim = coords.dim;
@@ -253,7 +254,7 @@ function getArrayExtent(coords) {
 	var maxX = Number.NEGATIVE_INFINITY;
 	var maxY = Number.NEGATIVE_INFINITY;
 
-	for(var i=0; i<len; i+=dim){
+	for (var i = 0; i < len; i += dim) {
 		var x = arr[i];
 		var y = arr[i+1];
 		minX = Math.min(minX, x);
